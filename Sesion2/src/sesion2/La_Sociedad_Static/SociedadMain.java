ad2daefe9b53b54d2494c215b7fc1f66c541150c
gustavo <gustavo@localhost.localdomain>
2014-05-02 15:11:06 -0500

    Segundo Commit

---
+++ Sesion2/src/sesion2/La_Sociedad_Static/SociedadMain.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sesion2.La_Sociedad_Static;
+
+/**
+ *
+ * @author gustavo
+ */
+public class SociedadMain {
+    
+}
